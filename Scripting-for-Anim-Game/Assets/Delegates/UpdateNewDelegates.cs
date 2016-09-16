792506f72bc18093df64603400eeea202ffc1899
Scud <julieclingo@gmail.com>
2016-09-16 13:53:33 -0600

    Delegates Practice in Class

---
+++ Scripting-for-Anim-Game/Assets/Delegates/UpdateNewDelegates.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UpdateNewDelegates : MonoBehaviour {
+
+    public int health = 250;
+    //Actions are a type of delegate. Delegates contain functions just as vars contain data
+    Action DisplayHealth;
+    Action KillThePlayer;
+    Action EndTheGame;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Assign function DisplayHealthHandler to the action DisplayHealth
+        DisplayHealth = DisplayHealthHandler;
+	}
+
+    void EndGameHandler()
+    {
+        print("You is dead. Like, so dead.");
+        EndTheGame = null;
+    }
+
+    void KillPlayerHandler()
+    {
+        health--;
+        print(health);
+        if (health < 0)
+        {
+            KillThePlayer = null;
+            EndTheGame = EndGameHandler;
+        }
+    }
+
+    void DisplayHealthHandler()
+    {
+        print("Health is good.");
+        //Unassign all functions from DisplayHealth
+        DisplayHealth = null;
+        KillThePlayer = KillPlayerHandler;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //We check to see if any function is assigned to DisplayHealth
+        //If nothing is assigned, DisplayHealth will not run
+        if (DisplayHealth != null)        
+            DisplayHealth();
+
+        if (KillThePlayer != null)
+            KillThePlayer();
+
+        if (EndTheGame != null)
+            EndTheGame();       
+	}
+}
---
+++ Scripting-for-Anim-Game/Assets/Scripts/HotPockets.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotPockets : MonoBehaviour {
+
+	private int neutralScum = 2;
+
+	//LONG LIVE THE EMPEROR
+		
+	// Update is called once per frame
+	void Update () 
+
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			int loyaltyMeter = Random.Range (0, 4); 
+
+			//print (loyaltyMeter);
+
+			if (loyaltyMeter > neutralScum) 
+			{
+				print ("The Emperor would be proud.");
+			} 
+
+			else if (loyaltyMeter < neutralScum) 
+			{
+				print ("You Rebel scum.");
+			}
+
+			else
+			{
+				print ("You nerf herder, pick a side!");
+			}
+		}
+	}		
+}
