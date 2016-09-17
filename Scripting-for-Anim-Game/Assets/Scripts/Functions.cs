7fc8a16aad8b028c5b9b40101bdf378ca1f6c4ea
Scud <julieclingo@gmail.com>
2016-09-16 22:59:16 -0600

    Functions Assignment

---
+++ Scripting-for-Anim-Game/Assets/Scripts/Functions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Functions : MonoBehaviour {
+
+	private int ThisBeANumber = 10;
+
+	// Use this for initialization
+	void Update () 
+	{		
+		if (Input.GetKeyDown(KeyCode.Space) && (ThisBeANumber > 0)) 
+		{
+			ThisBeANumber--;
+			ThisBeAFunction ();
+		}
+	}
+	
+	// Update is called once per frame
+	void ThisBeAFunction () 
+	{
+		print (ThisBeANumber);
+
+		if (ThisBeANumber <= 0) 
+		{
+			print ("It's done, stop pressing Space.");
+		}
+	}
+}
