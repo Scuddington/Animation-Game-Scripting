fb3cd9694af57334586933965db1ccf93d3915e1
Scud <julieclingo@gmail.com>
2016-09-19 13:56:46 -0600

    Recycle Level

---
+++ Scripting-for-Anim-Game/Assets/Scripts/Level Scripts/RecycleLevelObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecycleLevelObject : MonoBehaviour {
+
+    private Vector3 newLocation;
+
+	void OnTriggerEnter()
+    {
+        newLocation.x = StaticVars.nextSectionPos;
+        transform.position = newLocation;
+        StaticVars.nextSectionPos += StaticVars.distance;
+        print("I'm hit!");
+    }
+
+}
