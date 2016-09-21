1a3cf7cd26d7467434528e9e75b514695fd70a0e
Scud <julieclingo@gmail.com>
2016-09-21 14:05:07 -0600

    GAME NOT WORK ARGGGG

--- Scripting-for-Anim-Game/Assets/Scripts/Level Scripts/RecycleLevelObject.cs
+++ Scripting-for-Anim-Game/Assets/Scripts/Level Scripts/RecycleLevelObject.cs
@@ -1,16 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class RecycleLevelObject : MonoBehaviour {
 
-    private Vector3 newLocation;
 
-	void OnTriggerEnter()
+    private Vector3 newLocation;
+    public List<Recycler> recyblableList;
+    private int i = 0;
+
+    void Start ()
     {
+        recyblableList = new List<Recycler>();
+        Recycler.recycleAction += RecycleActionHandler;
+    }
+
+    private void RecycleActionHandler(Recycler _r)
+    {
+        recyblableList.Add(_r);
+    }
+
+    void OnTriggerEnter()
+    {
+        
         newLocation.x = StaticVars.nextSectionPos;
-        transform.position = newLocation;
+        recyblableList[i].cube.position = newLocation;
         StaticVars.nextSectionPos += StaticVars.distance;
         print("I'm hit!");
+
+        if (i < recyblableList.Count)
+            i++;
     }
 
 }
