97d78cd614056a97ff29d9d204964aa4f370b2bd
Scud <julieclingo@gmail.com>
2016-09-20 23:51:09 -0600

    Loops Assignment

---
+++ Scripting-for-Anim-Game/Assets/Scripts/Assignment Scripts/Loops.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour {
+
+	private int forLoop;
+
+	// Use this for initialization
+//	void Start () 
+//	{
+//		bool moving = true;
+//
+//		do 
+//		{
+//			
+//		}
+//	}
+
+//	void ForLoopFunction ()
+//	{
+//		if (Input.GetKeyDown (KeyCode.A)) 
+//		{
+//			forLoop = 1;
+//		}
+//		else if (Input.GetKeyDown (KeyCode.S)) 
+//		{
+//			forLoop = 2;
+//		}
+//	}
+}
