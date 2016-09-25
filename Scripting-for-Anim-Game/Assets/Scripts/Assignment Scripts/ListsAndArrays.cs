8a4f1d8218f95428ad6dfbd86e445143a874a47c
Scud <julieclingo@gmail.com>
2016-09-24 23:06:56 -0600

    Arrays and Lists

---
+++ Scripting-for-Anim-Game/Assets/Scripts/Assignment Scripts/ListsAndArrays.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListsAndArrays : MonoBehaviour {
+
+	int [] theArray = new int[3];
+
+	public List<> Fruit;
+	// Use this for initialization
+	void Start () 
+	{
+		
+		List<Bananas> bananas = new List<Bananas>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
