8c1fc24cf8380aac41565c25575d147a8ffbc9e4
Scud <julieclingo@gmail.com>
2016-10-03 13:56:45 -0600

    Player Flip Rotate

---
+++ Scripting-for-Anim-Game/Assets/CharacterFlipArt.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFlipArt : MonoBehaviour {
+
+    public Transform characterArt;
+
+    public bool forward = true;
+	
+    void FlipCharacter(bool _b)
+    {
+        characterArt.Rotate(0, 180, 0);
+        forward = _b;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.A) && forward)
+        {
+            FlipCharacter(false);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && !forward)
+        {
+            FlipCharacter(true);
+        }
+    }
+}
