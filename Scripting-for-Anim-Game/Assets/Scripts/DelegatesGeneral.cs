e9a5cf2800d0f537d0c946bdb645ba02ec46b5df
Scud <julieclingo@gmail.com>
2016-10-14 13:57:03 -0600

    Delegate practice

---
+++ Scripting-for-Anim-Game/Assets/Scripts/DelegatesGeneral.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DelegatesGeneral : MonoBehaviour {
+
+    Action<DelegatesGeneral> Move;
+    Action Idle;
+    Action Jump;
+
+    int speed = 200;
+
+    void MoveHandler(DelegatesGeneral obj)
+    {
+        print("Moving at " + obj.speed + " miles per hour");
+        Move = null;
+        Idle = IdleHandler;
+    }
+    void IdleHandler()
+    {
+        print("Idling");
+        Idle = null;
+        Jump = JumpHandler;
+    }
+    void JumpHandler()
+    {
+        print("Jumping");
+        Jump = null;
+    }
+
+    void Start ()
+    {
+        Move = MoveHandler;
+        //Idle = IdleHandler;
+        //Jump = JumpHandler;
+    }
+
+    void Update ()
+    {
+        if (Move != null)
+            Move(this);
+        if (Idle != null)
+            Idle();
+        if (Jump != null)
+            Jump();
+    }
+}
