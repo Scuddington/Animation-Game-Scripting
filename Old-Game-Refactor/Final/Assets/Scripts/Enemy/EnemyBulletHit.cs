a2f4b959ce3d50b11b90a570451d2d11555e5b2a
Scud <julieclingo@gmail.com>
2016-10-15 00:02:21 -0600

    Old Game Refactor 03
    Removed all tags but one (prefabs only take prefab GameObject)
    Find workaround

--- Old-Game-Refactor/Final/Assets/Scripts/Enemy/EnemyBulletHit.cs
+++ Old-Game-Refactor/Final/Assets/Scripts/Enemy/EnemyBulletHit.cs
@@ -4,19 +4,15 @@ using System.Collections;
 public class EnemyBulletHit : MonoBehaviour {
 
 	public int bulletDamage = 10;
-
 	public int healthValue = 10;
-	
-	Animator anim;
 	public GameObject player;
 	PlayerHealth playerHealth;
-	
-	
+
 	void Start()
 	{
+		//LAST TAG: working on a fix
 		player = GameObject.FindGameObjectWithTag ("Player");
 		playerHealth = player.GetComponent<PlayerHealth>();
-		
 	}
 	
 	void OnCollisionEnter (Collision other)
--- Old-Game-Refactor/Final/Assets/Scripts/Player/PlayerShoot.cs
+++ Old-Game-Refactor/Final/Assets/Scripts/Player/PlayerShoot.cs
@@ -10,8 +10,7 @@ public class PlayerShoot : MonoBehaviour
 	int ammoValue = 1;
 
 	public GameObject player;
-	
-	public PlayerHealth playerHealth;
+	PlayerHealth playerHealth;
 
 	void Start()
 	{
@@ -23,13 +22,11 @@ public class PlayerShoot : MonoBehaviour
 	{
 		if (Input.GetKeyDown (KeyCode.Space) && AmmoManager.ammo > 0)
 		{
-			print ("Shoot HAAAAA");
 			Rigidbody instantiateProjectile = Instantiate (projectile, transform.position, transform.rotation)as Rigidbody;
 			
 			//instantiate bullet from PreFabs 
 			
 			instantiateProjectile.velocity = transform.TransformDirection (new Vector3 (0, 0, bulletSpeed));
-
 			playerHealth.ShootBullet (addBullet);
 			AmmoManager.ammo -= ammoValue;
 		}		
