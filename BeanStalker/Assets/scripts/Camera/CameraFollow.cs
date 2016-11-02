1be200a6d0b3f64067b983822d476ce07e26cbc1
Scud <julieclingo@gmail.com>
2016-11-02 14:00:39 -0600

    Bean stalker 01

---
+++ BeanStalker/Assets/scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float cameraSpeed = 1;
+    private Vector3 cameraPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //MoveCamera();
+	}
+	
+	IEnumerator MoveCamera()
+    {
+        cameraPos.y = cameraSpeed * Time.deltaTime;
+        transform.Translate(cameraPos);
+        print("Bwah");
+    }
+}
