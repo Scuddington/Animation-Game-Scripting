26032db7c34ea98fe0ab7c5e4ee3df077d51f3cd
Scud <julieclingo@gmail.com>
2016-11-09 13:59:28 -0700

    You're grounded

---
+++ Scripting-for-Anim-Game/Assets/Scripts/Grounding.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Grounding : MonoBehaviour {
+
+    public Vector3 startPoint;
+    private CharacterController cc;
+    private float gravity = 5;
+    private Vector3 tempP;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //gravity *= -1;
+        startPoint = transform.position;
+        cc = GetComponent<CharacterController>();
+    }
+	
+    void OnTriggerEnter ()
+    {
+        transform.position = startPoint;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        tempP.y = -gravity;
+        cc.Move(tempP * Time.deltaTime);
+        if (cc.isGrounded)
+        {
+            tempP.x = gravity* 2;
+        }
+        //else
+        //{
+        //    tempP.x = 0;
+        //}
+	}
+}
