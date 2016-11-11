84a30299e1bafb14442bacbbd9744d2047da6f33
Scud <julieclingo@gmail.com>
2016-11-11 13:56:50 -0700

    Jump across NavMesh

--- Scripting-for-Anim-Game/Assets/Scripts/Enemies/EnemyAgent.cs
+++ Scripting-for-Anim-Game/Assets/Scripts/Enemies/EnemyAgent.cs
@@ -6,14 +6,20 @@ public class EnemyAgent : MonoBehaviour {
 
     public NavMeshAgent myAgent;
     public Transform player;
+    public Animator myAnims;
 
 	// Use this for initialization
 	void Start () {
-	
+        myAgent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         myAgent.destination = player.position;
+        print(myAgent.isOnOffMeshLink);
+        if (myAgent.isOnOffMeshLink)
+        {
+            myAnims.SetBool("jump", true);
+        }
 	}
 }
