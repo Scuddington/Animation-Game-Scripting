d12d67e4f9c743dbbe02038ffe583dc83dcfa44b
Scud <julieclingo@gmail.com>
2016-11-14 20:28:21 -0700

    Impacts

---
+++ New Unity Project/Assets/MeleeHit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeHit : MonoBehaviour {
+
+    public GameObject impactParticle;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+            Instantiate(impactParticle, contact.point, Quaternion.identity);
+    }
+}
