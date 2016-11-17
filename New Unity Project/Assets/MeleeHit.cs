38206bc03fa157bba1cc1b9c49783e38573c4b45
Scud <julieclingo@gmail.com>
2016-11-17 00:23:46 -0700

    Better impacts and trail renderer

--- New Unity Project/Assets/MeleeHit.cs
+++ New Unity Project/Assets/MeleeHit.cs
@@ -5,9 +5,17 @@ public class MeleeHit : MonoBehaviour {
 
     public GameObject impactParticle;
 
+//	private bool hasCollided = false;
+
     void OnCollisionEnter(Collision collision)
     {
         foreach (ContactPoint contact in collision.contacts)
             Instantiate(impactParticle, contact.point, Quaternion.identity);
+
+//		if (!hasCollided)
+//		{
+//			hasCollided = true;
+//			Destroy (impactParticle);
+//		}
     }
 }
