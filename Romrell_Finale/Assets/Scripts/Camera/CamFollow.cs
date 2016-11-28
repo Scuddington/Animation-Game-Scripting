f1a2274c68d4f312df7a39327948505d87a0e3aa
Scud <julieclingo@gmail.com>
2016-11-28 13:57:57 -0700

    Move without update

---
+++ Romrell_Finale/Assets/Scripts/Camera/CamFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+    public Transform player;
+
+	void Update ()
+    {
+	    transform.position = new Vector3 (player.position.x, 14, player.position.z);
+	}
+}
--- Romrell_Finale/Assets/Scripts/Character/PlayerMove.cs
+++ Romrell_Finale/Assets/Scripts/Character/PlayerMove.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
+    private Vector3 playerPos;
+    private CharacterController playerCC;
+    public int speed = 2;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        playerCC = GetComponent<CharacterController>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKey(KeyCode.A))
+        {
+            StartCoroutine(Move());
+        }
 	}
+
+    IEnumerator Move ()
+    {
+        playerPos.x = speed * Input.GetAxis("Horizontal");
+        playerCC.Move(playerPos * Time.deltaTime);
+        yield return null;
+    }
 }
